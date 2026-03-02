32336c8f1be5bc2d625685cd9e5e53939bc0c69f
sourav-m619 <souravmandal032@gmail.com>
2026-03-02 22:59:13 +0530

    adding wip

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -7,8 +7,7 @@ variable "iam_roles" {
     type = list(string)
     default = [ "roles/storage.admin",
      "roles/iam.serviceAccountUser",
-     "roles/compute.admin",
-     "roles/iam.serviceAccounts.create"]
+     "roles/compute.admin"]
 }
 
 variable "role_sa_wip_impersonate" {
