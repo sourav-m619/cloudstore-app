f7c98750e488484370c879c6755b0ed4f6a3f196
sourav-m619 <souravmandal032@gmail.com>
2026-03-05 14:46:14 +0530

    adding artifact registry

---
+++ terraform/artifact-registry.tf
@@ -0,0 +1,21 @@
+resource "google_artifact_registry_repository" "cloudstore-repo" {
+  project = var.project_id
+  location      = var.region
+  repository_id = "cloudstore-image-repo"
+  description   = "This Repo consists of cloudstore app Images"
+  format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-last-10"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 10
+    }
+  }
+}
+
+# Output — needed in GitHub Actions CI/CD
+output "registry_url" {
+  description = "Use this URL in GitHub Actions to push images"
+  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.cloudstore-repo.repository_id}"
+}
