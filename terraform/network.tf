3e9f48d5393923be92302c82c64ee03a409cc9bd
sourav-m619 <souravmandal032@gmail.com>
2026-03-05 12:04:30 +0530

    creating router and NAT

--- terraform/network.tf
+++ terraform/network.tf
@@ -38,7 +38,7 @@ resource "google_compute_router" "nat-router-cs" {
 resource "google_compute_router_nat" "nat-cs" {
   name                               = "cs-nat"
   router                             = google_compute_router.nat-router-cs.name
-  region                             = google_compute_router.router.region
+  region                             = google_compute_subnetwork.subnet-cs.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
