c817f5d3cb2c45d907dc048908121afd3599c6b1
sourav-m619 <souravmandal032@gmail.com>
2026-03-07 17:25:42 +0530

    creating DB, User, secret manager

--- terraform/secret-manager.tf
+++ terraform/secret-manager.tf
@@ -1,13 +1,13 @@
-resource "google_secret_manager_secret" "db_password" {
-  project = var.project_id
-  secret_id = "db-password"
-  replication {
-    auto {}
-  }
-}
+# resource "google_secret_manager_secret" "db_password" {
+#   project = var.project_id
+#   secret_id = "db-password"
+#   replication {
+#     auto {}
+#   }
+# }
 
-# Actual password value stored in secret
-resource "google_secret_manager_secret_version" "db_password" {
-  secret      = google_secret_manager_secret.db_password.id
-  secret_data = var.db_password
-}
+# # Actual password value stored in secret
+# resource "google_secret_manager_secret_version" "db_password" {
+#   secret      = google_secret_manager_secret.db_password.id
+#   secret_data = var.db_password
+# }
